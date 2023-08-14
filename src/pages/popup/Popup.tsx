f1e678c930cf7c06d1cc140f81f1646bce68a1a1
Janine Sebastian <janinems42@gmail.com>
2023-08-14 12:02:41 -0400

    reloads from past sheet entry

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -11,10 +11,14 @@ import "@pages/popup/Popup.css";
 
 function Popup() {
 
+  // a unique initialized value is needed for the changeable variables here because the past values are called from storage
+  // before the states are initialized. so when the states are initialized, they trigger the effect hook that rewrites what is in
+  // storage. to prevent the initial values from overwriting the actual storage, they need unique values so special cases can be made.
+  // there was probably a better way to handle that tbh. 
+
   const [isLeet, setIsLeet] = useState(false);
   const [problemName, setName] = useState(""); // string
   const [isFave, setFave] = useState(-1); // kinda of a boolean: -1/0 = false; 1 = true
-  
   const [isSolved, setSolved] = useState(false); //boolean
   const [difficulty, setDifficulty] = useState(""); //string
   const [taglist, setTags] = useState(["  "]); // array of strings
@@ -51,17 +55,22 @@ function Popup() {
       chrome.tabs.sendMessage(tab.id, {text: "load"}, (response) => {
           if (response?.p_isleet){
             console.log("this is a leetpage.");
+            setIsLeet(true);
           chrome.storage.session.get({p_name: "", p_isfave: -1, p_solved: false, p_difficulty: "", p_tags: ["  "], p_tcomp: "  ", p_scomp: "  ", p_notes: "  "}).then((stuff) => {
             console.log("get something or nothing from session storage");
             if (response.p_name === stuff.p_name || response.p_name === " x ") {
+              // data in storage is from the same problem --> displays old data
+              // OR this is a leet problem page, but it doesn't indicate the problem (i.e. editorial/solutions/submissions) -->
+              // infers that it is the same problem that is from storage
               console.log("loading from old data, same problem");
               
-              setIsLeet(true);
               setName(stuff.p_name);
               setFave(stuff.p_isfave);
+              // makes sure that if the webpage says the problem is solved, it isn't overwritten by what is said in memory
               if (response.p_solved)
                 setSolved(response.p_solved);
-              else 
+              // still needs identify cases where it is booted from storage becase there are webpages that don't indicate p_solved
+              else  
                 setSolved(stuff.p_solved)
               setDifficulty(stuff.p_difficulty);
               setTags(stuff.p_tags);
@@ -71,15 +80,32 @@ function Popup() {
               console.log(response.p_solved);
               refresh();
             } else {
-              console.log("getting new data, new problem");
-              setIsLeet(response.p_isleet);
+              // data in storage does not match the problem on the page --> check sheet if new problem has a past entry -->
+              // load past entry (if there) or new data
+
               setName(response.p_name);
               setDifficulty(response.p_difficulty);
-              setTags(response.p_tags);
               setSolved(response.p_solved);
+
+              console.log("this is a new problem. checking for sheet for past entries")
+
+               chrome.runtime.sendMessage({ message: 'reload', p_name: response.p_name }, (reload) => {
+                 if (reload.result === 0) {
+                  console.log("reloading from past entry");
+                  loadFromSheet(reload.data, response.url);
+                  
+
+                 } else {
+                  console.log("getting new data, new problem");
+                  setTags(response.p_tags);
+    
+                  //save new data + other things as default states
+                  chrome.storage.session.set({ url: response.url, p_name: response.p_name, p_isfave: -1, p_solved: response.p_solved,
+                    p_difficulty: response.p_difficulty, p_tags: response.p_tags, p_tcomp: "", p_scomp: "", p_notes: "" }, () => {console.log("saved new problem");});
+                 }
+               });
+
               
-              chrome.storage.session.set({ url: response.url, p_name: response.p_name, p_isfave: -1, p_solved: response.p_solved,
-                p_difficulty: response.p_difficulty, p_tags: response.p_tags, p_tcomp: "", p_scomp: "", p_notes: "" }, () => {console.log("saved new problem");});
             }
           })
         }
@@ -88,6 +114,19 @@ function Popup() {
 
   }
 
+  function loadFromSheet (row, url) {
+    const s_fave = (row[8]=== "TRUE") ? 1 : 0;
+    setFave(s_fave);
+    setTags(row[2].split(","));
+    setTimeComp(row[4]);
+    setSpaceComp(row[5]);
+    setNotes(row[6]);
+    chrome.storage.session.set({ url: url, p_name: row[0], p_isfave: (row[8] === "TRUE" ? 1 :0), p_solved: (row[3] === "TRUE" ? 1 :0),
+                    p_difficulty: row[1], p_tags: row[2].split(","), p_tcomp: row[4], p_scomp: row[5], p_notes: row[6] }, () => {console.log("saved new problem");});
+    
+  }
+
+  // run loadStuff on bootup
   useEffect(() => {loadStuff();}, []);
 
   useEffect(() => {
@@ -97,17 +136,17 @@ function Popup() {
 
   useEffect(() => {
     if (notes != "  ")
-    chrome.storage.session.set({p_notes: notes}, () => { console.log("notes="+notes); });
+    chrome.storage.session.set({p_notes: notes.trim()}, () => { console.log("notes="+notes); });
   }, [notes]);
 
   useEffect(() => {
     if (timeComp != "  ")
-    chrome.storage.session.set({p_tcomp: timeComp}, () => { console.log("timeComp="+timeComp); });
+    chrome.storage.session.set({p_tcomp: timeComp.trim()}, () => { console.log("timeComp="+timeComp); });
   }, [timeComp]);
 
   useEffect(() => {
     if (spaceComp != "  ")
-    chrome.storage.session.set({p_scomp: spaceComp}, () => { console.log("spaceComp="+spaceComp); });
+    chrome.storage.session.set({p_scomp: spaceComp.trim()}, () => { console.log("spaceComp="+spaceComp); });
   }, [spaceComp]);
 
   useEffect(() => {
@@ -126,7 +165,8 @@ function Popup() {
         <img src={logo} alt="logo"/>
         <strong>LeetTracker</strong>
       </header>
-      { isLeet &&
+      { // display when the page is a leetcode problem 
+      isLeet &&
         (
         <>
         
@@ -141,7 +181,7 @@ function Popup() {
         </>
         )
       }
-      {
+      { // display when there is not leetcode problem
         !isLeet &&
         (
           <div className="no-leet">
